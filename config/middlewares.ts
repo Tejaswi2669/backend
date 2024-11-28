2f4753c2c83102d1e17e9ec6957ee3b045b18ccd
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-11-28 03:57:06 +0100

    form table creation

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,20 @@
+// backend/config/middlewares.ts
+
 export default [
-  'strapi::logger',
+  // Strapi default middlewares
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Add your Ionic frontend URL here
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
